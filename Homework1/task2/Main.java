c85e42756cc1e3b80631fec0af960a86ce860278
maryy969 <114599920+maryy969@users.noreply.github.com>
2022-09-28 20:39:08 +0300

    Create Main.java

---
+++ Homework1/task2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 0;
+        short s = 0;
+        int a = 0;
+        long l = 966448L;
+        float f = 110.0f;
+        double d = 14.2;
+        boolean bool = true;
+        char c = 'c';
