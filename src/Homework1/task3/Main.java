f75ac31aab2f52c64419ec3f86a9fb26e40b2e22
maryy969 <114599920+maryy969@users.noreply.github.com>
2022-10-02 18:09:47 +0300

    Create Main.java

---
+++ src/Homework1/task3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+     public static double myFunc(int a, int b, int c, int d) {
+         return (a * (b + (c / d)));
+     }
+    public static void main(String[] args) {
+         System.out.println(myFunc(2,5,7,8));
+
+
+    }
+ }
