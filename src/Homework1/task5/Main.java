b8223da71be149b2133d12c91924c0de96e0de53
maryy969 <114599920+maryy969@users.noreply.github.com>
2022-10-02 19:07:09 +0300

    Create Main.java

---
+++ src/Homework1/task5/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void PositiveCheck(int a) {
+        if (a>=0){
+            System.out.println("Positive");
+
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+    public static void main(String[] args) {
+        PositiveCheck(6);
+    }
+}
