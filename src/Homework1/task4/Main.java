36503b29166267c9fca0dd85be17677912114a38
maryy969 <114599920+maryy969@users.noreply.github.com>
2022-10-02 19:05:30 +0300

    Create Main.java

---
+++ src/Homework1/task4/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static boolean myComparison(int a,int b){
+        return((a+b>=10) && (a+b<=20));
+
+    }
+    public static void main(String[] args) {
+        System.out.println(myComparison(3,4));
+
+
+    }
+}
