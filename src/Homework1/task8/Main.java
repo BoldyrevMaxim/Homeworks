7f1058379e2d5ab857e0536e9459a719ea110140
maryy969 <114599920+maryy969@users.noreply.github.com>
2022-10-02 19:47:12 +0300

    Create Main.java

---
+++ src/Homework1/task8/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void isLeap(int year) {
+        if (year % 400 == 0) {
+            System.out.println("It's leap year");
+        } else if (year % 100 == 0) {
+            System.out.println("It's not leap year");
+        } else if (year % 4 == 0) {
+            System.out.println("It's leap year");
+    } else
+
+    {
+        System.out.println("It's not leap year");
+    }
+
+   }
+    public static void main(String[] args) {
+        isLeap(2023);
+    }
+}
