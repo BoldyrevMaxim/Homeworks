9d26d7ca2cf913f61d9d0268bf22d90c81f05383
maryy969 <114599920+maryy969@users.noreply.github.com>
2022-10-02 19:15:00 +0300

    Create Main.java

---
+++ src/Homework1/task6/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+     public static boolean NegativeCheck(int a){
+         return (a<0);
+     }
+    public static void main(String[] args) {
+         System.out.println(NegativeCheck(4));
+    }
+}
